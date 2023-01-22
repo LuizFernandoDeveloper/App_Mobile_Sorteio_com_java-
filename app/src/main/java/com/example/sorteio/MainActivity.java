b3fefa4d5537e2ad257076394d82c85c076dee74
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-01-22 11:56:12 -0300

    Finalizando o App

--- app/src/main/java/com/example/sorteio/MainActivity.java
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sortearNumero(View view){
+        TextView textView = findViewById(R.id.resultado);
+        int numero = new Random().nextInt(11);
+
+        textView.setText("Numero: "+ numero);
+    }
+
+
 }
